24ae63eaa2d5020eda39eab32f9f050c4a4b149e
digvijay52 <digvijayjadeja62@gmail.com>
2019-01-11 15:15:38 +0530

    Displayed articles database through the html page

--- articles/views.py
+++ articles/views.py
@@ -2,9 +2,11 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from .models import Article
 
 def article_list(request):
-    return render(request,'articles/article_list.html')
+    articles = Article.objects.all().order_by('date')
+    return render(request,'articles/article_list.html',{'articles':articles})
 
 
 # Create your views here.
