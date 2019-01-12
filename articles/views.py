89b46f05868646640198381b011fed0271725380
digvijay52 <digvijayjadeja62@gmail.com>
2019-01-12 11:12:24 +0530

    Named URL

--- articles/views.py
+++ articles/views.py
@@ -3,10 +3,14 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from .models import Article
-
+from django.http import HttpResponse
 def article_list(request):
     articles = Article.objects.all().order_by('date')
     return render(request,'articles/article_list.html',{'articles':articles})
+
+def article_details(request,slug):
+    return HttpResponse(slug)   
+    
 
 
 # Create your views here.
