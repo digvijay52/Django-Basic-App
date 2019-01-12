f3fa6506a14213df779feb39afcc67ba2a8de6e7
digvijay52 <digvijayjadeja62@gmail.com>
2019-01-12 11:58:54 +0530

    Added the image attribute in the articles page

--- dnautic/urls.py
+++ dnautic/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url,include
 from django.contrib import admin
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -23,3 +26,6 @@ urlpatterns = [
     url(r'^about/$', views.about ),
     url(r'^$', views.homepage )
 ]
+
+urlpatterns+= staticfiles_urlpatterns()
+urlpatterns+= static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
